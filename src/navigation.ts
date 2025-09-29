9fe90a47129770528ea3b0cefde39816776f2eee
Tyler Savery <tyler@theyoungastronauts.com>
2025-09-29 11:01:37 -0400

    updated GUI download links

--- src/navigation.ts
+++ src/navigation.ts
@@ -20,9 +20,9 @@ export const externalLinks = {
   integrationSwaggerDocs: `${DOCS_BASE_URL}/docs/integration/swagger`,
 
   webWallet: `https://wallet.verifiedx.io`,
-  downloadGuiMacArm: 'https://github.com/VerifiedXBlockchain/VerifiedX-GUI/releases/download/5.0.7/VFX-OSX-ARM-Installer.dmg',
-  downloadGuiMacIntel: 'https://github.com/VerifiedXBlockchain/VerifiedX-GUI/releases/download/5.0.7/VFX-OSX-Intel-Installer.dmg',
-  downloadGuiWindows: 'https://github.com/VerifiedXBlockchain/VerifiedX-GUI/releases/download/5.0.7/VFXWalletSetup-64.exe',
+  downloadGuiMacArm: 'https://github.com/VerifiedXBlockchain/VerifiedX-GUI/releases/download/5.0.8/VFX-OSX-ARM-Installer.dmg',
+  downloadGuiMacIntel: 'https://github.com/VerifiedXBlockchain/VerifiedX-GUI/releases/download/5.0.8/VFX-OSX-Intel-Installer.dmg',
+  downloadGuiWindows: 'https://github.com/VerifiedXBlockchain/VerifiedX-GUI/releases/download/5.0.8/VFXWalletSetup-64.exe',
   downloadCli: 'https://github.com/VerifiedXBlockchain/VerifiedX-Core/releases/tag/beta5.0.6',
   trilliumIde: 'https://trillium.verifiedx.io',
   githubOrg: 'https://github.com/VerifiedXBlockchain',
